62d718c138fc4bbe8285d5bf68ff089761e2d52a
Sujay Chandra Shekara Sharma <sujaycs@instgpu-02.cs.wisc.edu>
2024-10-07 14:07:45 -0500

    Update first and second moments of gradients in optimizer

--- assignment2/optimizer.py
+++ assignment2/optimizer.py
@@ -38,20 +38,36 @@ class AdamW(Optimizer):
                 if grad.is_sparse:
                     raise RuntimeError("Adam does not support sparse gradients, please consider SparseAdam instead")
 
-                raise NotImplementedError()
-
                 # State should be stored in this dictionary
                 state = self.state[p]
 
+                if len(state) == 0:
+                    state["step"] = 0
+                    state["mean"] = torch.zeros_like(p.data)
+                    state["variance"] = torch.zeros_like(p.data)
+    
                 # Access hyperparameters from the `group` dictionary
                 alpha = group["lr"]
+                beta1, beta2 = group["betas"]
+                eps = group["eps"]
+                weight_decay = group["weight_decay"]
+
+                state["step"] += 1
+                step = state["step"]
 
                 # Update first and second moments of the gradients
+
+                state["mean"].lerp_(grad, 1 - beta1)
+                state["variance"].mul_(beta2).addcmul_(grad, grad, value=1 - beta2)
 
                 # Bias correction
                 # Please note that we are using the "efficient version" given in
                 # https://arxiv.org/abs/1412.6980
 
+                bias_correction1 = 1.0 - beta1**step
+                bias_correction2 = 1.0 - beta2**step
+
+                alpha_new = alpha * bias_correction2.sqrt() / bias_correction1
                 # Update parameters
 
                 # Add weight decay after the main gradient-based updates.
